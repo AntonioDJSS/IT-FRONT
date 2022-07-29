2e824a84f87c065584763e77739d12819ffa74e1
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-07-29 16:20:45 -0500

    Actualizado

--- src/pages/Validator.jsx
+++ src/pages/Validator.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Title from "../components/Title";
+import Filtrador from "../components/Filtrador";
 
 const Validator = () => {
   return (
     <>
       <Header />
       <Title />
+      <Filtrador />
       <Footer />
     </>
   );
