53dd2617e10b359fcb8a9c2fb3c7c4a780a6cfc6
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-08-01 15:12:58 -0500

    Actualizado

--- src/components/Filtrador.jsx
+++ src/components/Filtrador.jsx
@@ -87,42 +87,40 @@ function Filtrador({ data }) {
                           </th>
                         </tr>
                       </thead>
-                      <tbody className="min-w-full divide-y divide-gray-300">
-                        {filteredData.length != 0 && (
-                          <tr className="">
-                            {filteredData.slice(0, 15).map((value, key) => {
-                              return (
-                                <>
-                                  <div>
-                                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
-                                      <div className="flex items-center">
-                                        <div className="ml-4">
-                                          <div className="font-medium text-gray-900">
-                                            {value.title}
-                                          </div>
+                      {filteredData.length != 0 && (
+                        <tbody className="min-w-full divide-y divide-gray-300">
+                          {filteredData.slice(0, 15).map((value, key) => {
+                            return (
+                              <>
+                                <tr>
+                                  <td className=" whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                                    <div className="flex items-center">
+                                      <div className="ml-4">
+                                        <div className="font-medium text-gray-900">
+                                          {value.title}
                                         </div>
                                       </div>
-                                    </td>
-                                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                      <div className="text-gray-900">
-                                        {value.language}
-                                      </div>
-                                    </td>
-                                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                      <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
-                                        Activo
-                                      </span>
-                                    </td>
-                                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                      {value.pages}
-                                    </td>
-                                  </div>
-                                </>
-                              );
-                            })}
-                          </tr>
-                        )}
-                      </tbody>
+                                    </div>
+                                  </td>
+                                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <div className="text-gray-900">
+                                      {value.language}
+                                    </div>
+                                  </td>
+                                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                                      Activo
+                                    </span>
+                                  </td>
+                                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    {value.pages}
+                                  </td>
+                                </tr>
+                              </>
+                            );
+                          })}
+                        </tbody>
+                      )}
                     </table>
                   </div>
                 </div>
