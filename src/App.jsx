b31ec962a1a00eb2c91fbb9b47a8d7fa27837552
Deployments <107765334+IKTAN-ST@users.noreply.github.com>
2022-10-03 17:28:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashProvider } from "./context/DashProvider";
 
 import AuthLayout from "./layout/AuthLayout";
-import Inicio from "./pages/Inicio";
 import Validator from "./pages/Validator";
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
       <DashProvider>
         <Routes>
           <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Inicio />} />
-            <Route path="certified" element={<Validator />} />
+            <Route index element={<Validator />} />
           </Route>
         </Routes>
       </DashProvider>
