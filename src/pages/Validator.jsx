e96a4e8a39781242f7abd5219c611808f369449f
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-10-18 18:42:39 -0500

    Editor

--- src/pages/Validator.jsx
+++ src/pages/Validator.jsx
@@ -1,10 +1,15 @@
 import Footer from "../components/Footer";
 import Title from "../components/Title";
+import Filtrador from "../components/Filtrador";
+import useDash from "../hooks/useDash";
+import Area from "../components/Area";
 
 const Validator = () => {
   return (
     <>
       <Title />
+      <Area />
+      <Filtrador data={clientes} />
       <Footer />
     </>
   );
