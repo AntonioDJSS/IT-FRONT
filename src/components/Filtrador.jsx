0286f99d22a46b62ab9f7dec6167cca51b03e86f
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-10-18 19:07:42 -0500

    Actualizado

--- src/components/Filtrador.jsx
+++ src/components/Filtrador.jsx
@@ -134,6 +134,16 @@ function Filtrador({ data }) {
                             </div>
                           </div>
                         </div>
+                        <div className="mt-4 border border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-">
+                          <div className="px-4 py-5 sm:p-6">
+                            <h3 className="leading-6 text-gray-500 text-sm">
+                              Observaciones:
+                            </h3>
+                            <div className="uppercase mt-2 max-w-xl text-sm font-medium text-gray-900">
+                              <p>No tienes observaciones.</p>
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </>
                   );
